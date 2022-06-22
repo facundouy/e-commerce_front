eea7c3c5fc9d3ece6739a6e5746e4a83e1eccb53
facundouy <97556086+facundouy@users.noreply.github.com>
2022-06-22 17:20:23 -0300

    Single product page elements created

--- src/components/singleProduct/SingleProduct.jsx
+++ src/components/singleProduct/SingleProduct.jsx
@@ -1,24 +1,57 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import axios from "axios";
+import ReactStars from "react-rating-stars-component";
+import "./singleProduct.css";
 
 function SingleProduct() {
-  const [product, setProduct] = useState([]);
+  const [product, setProduct] = useState({});
+  const params = useParams();
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+  };
 
   useEffect(() => {
     async function getProduct() {
       try {
-        const response = await axios.get("");
-      } catch {}
+        // TODO: Sustituir por variables de entorno.
+        const response = await axios.get(
+          `http://localhost:3001/api/product/${params.id}`
+        );
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error cuando trae de la API:", error);
+      }
     }
-  });
+    getProduct();
+  }, []);
 
   return (
     <Container>
       <Row>
-        <Col className="bg-success">Culo</Col>
-        <Col className="bg-warning">Coso</Col>
+        <Col className="">
+          {/* TODO: Sustituir por variables de entorno. */}
+          <img
+            className="image"
+            src={`http://localhost:3001/images/${product.image}`}
+            alt={product.name}
+          />
+        </Col>
+        <Col className="">
+          <h2>{product.name}</h2>
+          <small>${product.price}</small>
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={24}
+            value={5}
+            activeColor="#c19d56"
+          />
+          <small>(Based on 38 reviews)</small>
+          <p>{product.description}</p>
+        </Col>
       </Row>
     </Container>
   );
