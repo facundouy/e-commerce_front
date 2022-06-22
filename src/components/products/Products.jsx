0a75bb50936fc22489fef86c9d90f65ac8e72095
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-22 15:32:17 -0300

    .env para react

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -6,56 +6,55 @@ import { useEffect, useState } from "react";
 import Axios from "axios";
 
 function Products() {
-  const [productList, setProductList] = useState([]);
-  useEffect(() => {
-    const getProducts = async () => {
-      try {
-        const response = await Axios.get("http://localhost:3001/api/products");
-        setProductList(response.data);
-        console.log("-------", productList);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    getProducts();
-  }, []);
-  return (
-    <>
-      <div className="products-container">
-        <ul className="products-list">
-          {productList &&
-            productList.map((product) => {
-              return (
-                <div className="card-product">
-                  <img
-                    src={`http://localhost:3001/images/${product.image}`}
-                    alt="cake cover"
-                  />
-                  <div className="product-info">
-                    <h6>{product.name.toUpperCase()}</h6>
-                    <span className="product-category">
-                      {product.category.name}
-                    </span>
-                    <span className="product-price">${product.price}</span>
-                  </div>
-                </div>
-              );
-            })}
-        </ul>
-      </div>
-      <div className="side-container">
-        <div className="side-price-filter">
-          <h6>PRICE FILTER</h6>
-        </div>
-        <div className="side-categories">
-          <h6>CATEGORIES</h6>
-        </div>
-        <div className="side-instagram">
-          <h6>INSTRAGRAM</h6>
-        </div>
-      </div>
-    </>
-  );
+	const [productList, setProductList] = useState([]);
+	useEffect(() => {
+		const getProducts = async () => {
+			try {
+				const response = await Axios.get("http://localhost:3001/api/products");
+				setProductList(response.data);
+			} catch (error) {
+				console.log(error);
+			}
+		};
+		getProducts();
+	}, []);
+	return (
+		<>
+			<div className='products-container'>
+				<ul className='products-list'>
+					{productList &&
+						productList.map((product) => {
+							return (
+								<div className='card-product'>
+									<img
+										src={`${process.env.REACT_APP_BACKEND_URL}/images/${product.image}`}
+										alt='cake cover'
+									/>
+									<div className='product-info'>
+										<h6>{product.name.toUpperCase()}</h6>
+										<span className='product-category'>
+											{product.category.name}
+										</span>
+										<span className='product-price'>${product.price}</span>
+									</div>
+								</div>
+							);
+						})}
+				</ul>
+			</div>
+			<div className='side-container'>
+				<div className='side-price-filter'>
+					<h6>PRICE FILTER</h6>
+				</div>
+				<div className='side-categories'>
+					<h6>CATEGORIES</h6>
+				</div>
+				<div className='side-instagram'>
+					<h6>INSTRAGRAM</h6>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Products;
