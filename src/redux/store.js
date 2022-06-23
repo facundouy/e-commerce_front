79056aa3b0132ca9642285967c6c8b8b1d9df545
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-23 10:39:25 -0300

    count reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
+import countReducer from "./countSlice";
 
 export const store = configureStore({
-	reducer: {
-		user: userReducer,
-	},
+  reducer: {
+    user: userReducer,
+    count: countReducer,
+  },
 });
