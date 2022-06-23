d6aba3285ae8928d3d4b24e923f5fb84b26c524d
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-23 12:04:33 -0300

    Nav update

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -1,54 +1,48 @@
 import "./nav.css";
-
-import {
-	Navbar,
-	Nav,
-	Form,
-	NavDropdown,
-	Container,
-	FormControl,
-	Button,
-} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import { GiCupcake } from "react-icons/gi";
 
 function NavComp() {
-
-  return (
-    <Navbar bg="light" className="nav-container" sticky="top">
-      <Container fluid>
-        <Navbar.Brand href="/">Hack Shop</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
-            <Nav.Link className="item-nav-bar" href="/">
-              HOME
-            </Nav.Link>
-            <Nav.Link className="item-nav-bar" href="#action2">
-              ABOUT
-            </Nav.Link>
-            <NavDropdown
-              className="item-nav-bar"
-              title="CATEGORIES"
-              id="navbarScrollingDropdown"
-            >
-              <NavDropdown.Item href="#action3">Cakes</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Tarts</NavDropdown.Item>
-              <NavDropdown.Item href="#action5">Truffles</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link className="item-nav-bar" href="/shop">
-              SHOP
-            </Nav.Link>
-            <Nav.Link className="item-nav-bar" href="/contact">
-              CONTACT
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar bg='light' className='nav-container' sticky='top'>
+			<Container fluid>
+				<Link to='/' className='item-nav-bar'>
+					<Navbar.Brand href='#'>
+						<GiCupcake />
+					</Navbar.Brand>
+				</Link>
+				<Navbar.Toggle aria-controls='navbarScroll' />
+				<Navbar.Collapse id='navbarScroll'>
+					<Nav
+						className='me-auto my-2 my-lg-0'
+						style={{ maxHeight: "100px" }}
+						navbarScroll>
+						<Link to='/' className='item-nav-bar'>
+							<Nav.Item href='/'>HOME</Nav.Item>
+						</Link>
+						<Nav.Item className='item-nav-bar' href='#action2'>
+							ABOUT
+						</Nav.Item>
+						<NavDropdown
+							className='item-nav-bar'
+							title='CATEGORIES'
+							id='navbarScrollingDropdown'>
+							<NavDropdown.Item href='#action3'>Cakes</NavDropdown.Item>
+							<NavDropdown.Item href='#action4'>Tarts</NavDropdown.Item>
+							<NavDropdown.Item href='#action5'>Truffles</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Item className='item-nav-bar' href='/shop'>
+							SHOP
+						</Nav.Item>
+						<Nav.Item className='item-nav-bar' href='/contact'>
+							CONTACT
+						</Nav.Item>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default NavComp;
