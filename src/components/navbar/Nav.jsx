f62f9e023c14378a360da1f96bbd06dae7d25f98
flofal <florencia.falco93@gmail.com>
2022-06-23 14:25:32 -0300

    Update Nav.jsx

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -6,51 +6,59 @@ import { AiOutlineShopping } from "react-icons/ai";
 import { FaRegUser } from "react-icons/fa";
 
 function NavComp() {
-	return (
-		<Navbar bg='light' className='nav-container navbar-me-auto ' sticky='top'>
-			<Container fluid className='d-flex justify-content-between'>
-				<div>
-					<Link to='/' className='item-nav-bar'>
-						<Navbar.Brand href='#'>
-							<GiCupcake />
-						</Navbar.Brand>
-					</Link>
-				</div>
-				<div className='container-nav-items'>
-					<Navbar.Toggle aria-controls='navbarScroll' />
-					<Navbar.Collapse id='navbarScroll'>
-						<Nav
-							className='me-auto my-2 my-lg-0'
-							style={{ maxHeight: "100px" }}
-							navbarScroll>
-							<Link to='/' className='item-nav-bar'>
-								<Nav.Item href='/'>HOME</Nav.Item>
-							</Link>
-							<Nav.Item className='item-nav-bar' href='#action2'>
-								ABOUT
-							</Nav.Item>
-							<NavDropdown
-								className='item-nav-bar'
-								title='CATEGORIES'
-								id='navbarScrollingDropdown'>
-								<NavDropdown.Item href='#action3'>Cakes</NavDropdown.Item>
-								<NavDropdown.Item href='#action4'>Tarts</NavDropdown.Item>
-								<NavDropdown.Item href='#action5'>Truffles</NavDropdown.Item>
-							</NavDropdown>
-							<Nav.Item className='item-nav-bar' href='/shop'>
-								SHOP
-							</Nav.Item>
-							<Nav.Item className='item-nav-bar' href='/contact'>
-								CONTACT
-							</Nav.Item>
-							<FaRegUser />
-							<AiOutlineShopping />
-						</Nav>
-					</Navbar.Collapse>
-				</div>
-			</Container>
-		</Navbar>
-	);
+  return (
+    <Navbar bg="light" className="nav-container navbar-me-auto " sticky="top">
+      <Container fluid className="d-flex justify-content-between">
+        <div>
+          <Link to="/" className="item-nav-bar">
+            <Navbar.Brand href="#">
+              <GiCupcake />
+            </Navbar.Brand>
+          </Link>
+        </div>
+        <div className="container-nav-items">
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to="/" className="item-nav-bar">
+                <Nav.Item href="/">HOME</Nav.Item>
+              </Link>
+              <Link to="/about" className="item-nav-bar">
+                <Nav.Item className="item-nav-bar" href="#action2">
+                  ABOUT
+                </Nav.Item>
+              </Link>
+              <NavDropdown
+                className="item-nav-bar"
+                title="CATEGORIES"
+                id="navbarScrollingDropdown"
+              >
+                <NavDropdown.Item href="#action3">Cakes</NavDropdown.Item>
+                <NavDropdown.Item href="#action4">Tarts</NavDropdown.Item>
+                <NavDropdown.Item href="#action5">Truffles</NavDropdown.Item>
+              </NavDropdown>
+              <Link to="/shop" className="item-nav-bar">
+                <Nav.Item className="item-nav-bar" href="/shop">
+                  SHOP
+                </Nav.Item>
+              </Link>
+              <Link to="/contact" className="item-nav-bar">
+                <Nav.Item className="item-nav-bar" href="/contact">
+                  CONTACT
+                </Nav.Item>
+              </Link>
+              <FaRegUser />
+              <AiOutlineShopping />
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavComp;
