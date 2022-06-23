72fdf7fd89714c56f381d69de3a692f7699b4578
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-23 10:17:51 -0300

    singleProduct

--- src/components/singleProduct/SingleProduct.jsx
+++ src/components/singleProduct/SingleProduct.jsx
@@ -7,54 +7,52 @@ import ReactStars from "react-rating-stars-component";
 import "./singleProduct.css";
 
 function SingleProduct() {
-	const [product, setProduct] = useState(null);
-	const params = useParams();
-	const ratingChanged = (newRating) => {};
+  const [product, setProduct] = useState(null);
+  const params = useParams();
+  const ratingChanged = (newRating) => {};
 
-	useEffect(() => {
-		async function getProduct() {
-			try {
-				// TODO: Sustituir por variables de entorno.
-				const response = await axios.get(
-					`${process.env.REACT_APP_BACKEND_URL}/api/product/${params.id}`
-				);
-				setProduct(response.data);
-			} catch (error) {
-				console.error("Error cuando trae de la API:", error);
-			}
-		}
-		getProduct();
-	}, [params.id]);
+  useEffect(() => {
+    async function getProduct() {
+      try {
+        const response = await axios.get(
+          `${process.env.REACT_APP_BACKEND_URL}/api/product/${params.id}`
+        );
+        setProduct(response.data);
+      } catch (error) {
+        console.error("Error cuando trae de la API:", error);
+      }
+    }
+    getProduct();
+  }, [params.id]);
 
-	return product ? (
-		<Container>
-			<Row>
-				<Col className=''>
-					{/* TODO: Sustituir por variables de entorno. */}
-					<img
-						className='image'
-						src={`${process.env.REACT_APP_BACKEND_URL}/images/${product.image}`}
-						alt={product.name}
-					/>
-				</Col>
-				<Col className=''>
-					<h2>{product.name}</h2>
-					<small>${product.price}</small>
-					<ReactStars
-						count={5}
-						onChange={ratingChanged}
-						size={24}
-						value={5}
-						activeColor='#c19d56'
-					/>
-					<small>(Based on 38 reviews)</small>
-					<p>{product.description}</p>
-				</Col>
-			</Row>
-		</Container>
-	) : (
-		<p>Loading...</p>
-	);
+  return product ? (
+    <Container>
+      <Row>
+        <Col className="">
+          <img
+            className="image"
+            src={`${process.env.REACT_APP_BACKEND_URL}/images/${product.image}`}
+            alt={product.name}
+          />
+        </Col>
+        <Col className="">
+          <h2>{product.name}</h2>
+          <small>${product.price}</small>
+          <ReactStars
+            count={5}
+            onChange={ratingChanged}
+            size={24}
+            value={5}
+            activeColor="#c19d56"
+          />
+          <small>(Based on 38 reviews)</small>
+          <p>{product.description}</p>
+        </Col>
+      </Row>
+    </Container>
+  ) : (
+    <p>Loading...</p>
+  );
 }
 
 export default SingleProduct;
