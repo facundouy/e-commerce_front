89efd47b64de0b6e1d9b519a65d0d2ac76f578ba
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-23 12:54:53 -0300

    nav bar del infierno

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -5,41 +5,45 @@ import { GiCupcake } from "react-icons/gi";
 
 function NavComp() {
 	return (
-		<Navbar bg='light' className='nav-container' sticky='top'>
-			<Container fluid>
-				<Link to='/' className='item-nav-bar'>
-					<Navbar.Brand href='#'>
-						<GiCupcake />
-					</Navbar.Brand>
-				</Link>
-				<Navbar.Toggle aria-controls='navbarScroll' />
-				<Navbar.Collapse id='navbarScroll'>
-					<Nav
-						className='me-auto my-2 my-lg-0'
-						style={{ maxHeight: "100px" }}
-						navbarScroll>
-						<Link to='/' className='item-nav-bar'>
-							<Nav.Item href='/'>HOME</Nav.Item>
-						</Link>
-						<Nav.Item className='item-nav-bar' href='#action2'>
-							ABOUT
-						</Nav.Item>
-						<NavDropdown
-							className='item-nav-bar'
-							title='CATEGORIES'
-							id='navbarScrollingDropdown'>
-							<NavDropdown.Item href='#action3'>Cakes</NavDropdown.Item>
-							<NavDropdown.Item href='#action4'>Tarts</NavDropdown.Item>
-							<NavDropdown.Item href='#action5'>Truffles</NavDropdown.Item>
-						</NavDropdown>
-						<Nav.Item className='item-nav-bar' href='/shop'>
-							SHOP
-						</Nav.Item>
-						<Nav.Item className='item-nav-bar' href='/contact'>
-							CONTACT
-						</Nav.Item>
-					</Nav>
-				</Navbar.Collapse>
+		<Navbar bg='light' className='nav-container navbar-me-auto ' sticky='top'>
+			<Container fluid className='d-flex justify-content-between'>
+				<div>
+					<Link to='/' className='item-nav-bar'>
+						<Navbar.Brand href='#'>
+							<GiCupcake />
+						</Navbar.Brand>
+					</Link>
+				</div>
+				<div className='container-nav-items'>
+					<Navbar.Toggle aria-controls='navbarScroll' />
+					<Navbar.Collapse id='navbarScroll'>
+						<Nav
+							className='me-auto my-2 my-lg-0'
+							style={{ maxHeight: "100px" }}
+							navbarScroll>
+							<Link to='/' className='item-nav-bar'>
+								<Nav.Item href='/'>HOME</Nav.Item>
+							</Link>
+							<Nav.Item className='item-nav-bar' href='#action2'>
+								ABOUT
+							</Nav.Item>
+							<NavDropdown
+								className='item-nav-bar'
+								title='CATEGORIES'
+								id='navbarScrollingDropdown'>
+								<NavDropdown.Item href='#action3'>Cakes</NavDropdown.Item>
+								<NavDropdown.Item href='#action4'>Tarts</NavDropdown.Item>
+								<NavDropdown.Item href='#action5'>Truffles</NavDropdown.Item>
+							</NavDropdown>
+							<Nav.Item className='item-nav-bar' href='/shop'>
+								SHOP
+							</Nav.Item>
+							<Nav.Item className='item-nav-bar' href='/contact'>
+								CONTACT
+							</Nav.Item>
+						</Nav>
+					</Navbar.Collapse>
+				</div>
 			</Container>
 		</Navbar>
 	);
