1d21436ce5fe1d7b24e614939f078d7817a31829
flofal <florencia.falco93@gmail.com>
2022-06-24 11:27:03 -0300

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,31 +5,40 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div className="contact-container">
-        <h5>CONTACT</h5>
-        <p>Heinrichstrasse 267c / 3. Stock 8005 Zürich</p>
-        <p>E-mail: dolcino@qodeinteractive.com</p>
-        <p>Phone: +88(0) 101 0000 000</p>
-      </div>
-      <div className="center">
-        <div className="logo-center-footer">
-          <GiCupcake className="logo" />
-          <span>eat what you love</span>
-          <h4>CAKES SHOP</h4>
-          <img
-            src="https://dolcino.qodeinteractive.com/wp-content/uploads/2018/10/h1-slide-1-img-1.png"
-            alt="Separador" className="separator"
-          />
-          <p>
-            1926. - Year of beggining. There are many variations of passa ges of
-            Lorem Ipsum avai lable, but the majority suffered alter.
-          </p>
-          <div className="container-icons-footer"></div>
+    <>
+      <div className="footer-container">
+        <div className="contact-container">
+          <h5>CONTACT</h5>
+          <p>Av. Gral. Libertador 0000, Montevideo</p>
+          <p>E-mail: hello@cakes.com</p>
+          <p>Phone: +598 099 0000 000</p>
+        </div>
+        <div className="center-container">
+          <div className="logo-center-footer">
+            <GiCupcake className="logo" />
+            <span>eat what you love</span>
+            <h4>CAKES SHOP</h4>
+            <img
+              src="https://dolcino.qodeinteractive.com/wp-content/uploads/2018/10/h1-slide-1-img-1.png"
+              alt="Separador"
+              className="separator"
+            />
+            <p>
+              1926. - Year of beggining. There are many variations of passa ges
+              of Lorem Ipsum avai lable, but the majority suffered alter.
+            </p>
+            <div className="icons-container-footer"></div>
+          </div>
+        </div>
+        <div className="map-container-footer">
+          <img src="/map.png"></img>
         </div>
       </div>
-      <div></div>
-    </div>
+      <div className="footer-bottom">
+        © 2022 Santiago Fantoni, Paulina Irazusta, Facundo Irazusta & Florencia
+        Falco.
+      </div>
+    </>
   );
 };
 
