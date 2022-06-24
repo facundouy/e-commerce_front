eeb93696e05f41d4b83d6f3dbecda8805326f2b2
Santiago Fantoni <fantonisantiago@gmail.com>
2022-06-24 15:05:35 -0300

    update front
    -Nav achicado
    -Dropdown del nav mas bonito
    -Footer en el cart
    -Cart agregado imagen

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,36 +5,43 @@ import Table from "react-bootstrap/Table";
 import "./cart.css";
 
 function Cart() {
-  const cart = useSelector((state) => state.cart);
-  return (
-    <div>
-      <ShopBanner />
-      <div className="container">
-        <Table bordered hover className="table">
-          <thead>
-            <tr>
-              <th>PRODUCT</th>
-              <th>PRICE</th>
-              <th>QUANTITY</th>
-              <th>SUBTOTAL</th>
-            </tr>
-          </thead>
-          <tbody>
-            {cart.map((product) => {
-              return (
-                <tr key={product._id}>
-                  <td>{product.name}</td>
-                  <td>{product.price}</td>
-                  <td>{product.quantity}</td>
-                  <td>{product.price * product.quantity}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-    </div>
-  );
+	const cart = useSelector((state) => state.cart);
+	return (
+		<div>
+			<ShopBanner />
+			<div className='container'>
+				<Table bordered hover className='table'>
+					<thead>
+						<tr>
+							<th>PRODUCT</th>
+							<th>PRICE</th>
+							<th>QUANTITY</th>
+							<th>SUBTOTAL</th>
+						</tr>
+					</thead>
+					<tbody>
+						{cart.map((product) => {
+							return (
+								<tr key={product._id}>
+									<td>
+										<img
+											className='image-item-cart'
+											src={`${process.env.REACT_APP_BACKEND_URL}/images/${product.image}`}
+											alt='imagen'
+										/>
+										{product.name}
+									</td>
+									<td>{product.price}</td>
+									<td>{product.quantity}</td>
+									<td>{product.price * product.quantity}</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</Table>
+			</div>
+		</div>
+	);
 }
 
 export default Cart;
--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -6,59 +6,63 @@ import { AiOutlineShopping } from "react-icons/ai";
 import { FaRegUser } from "react-icons/fa";
 
 function NavComp() {
-  return (
-    <Navbar bg="light" className="nav-container navbar-me-auto " sticky="top">
-      <Container fluid className="d-flex justify-content-between">
-        <div className="logo-container">
-          <Link to="/">
-            <Navbar.Brand>
-              <GiCupcake className="logo" />
-            </Navbar.Brand>
-          </Link>
-        </div>
-        <div className="container-nav-items">
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Link to="/" className="item-nav-bar">
-                <Nav.Item>HOME</Nav.Item>
-              </Link>
-              <Link to="/about" className="item-nav-bar">
-                <Nav.Item className="item-nav-bar">ABOUT</Nav.Item>
-              </Link>
-              <NavDropdown
-                className="item-nav-bar"
-                title="CATEGORIES"
-                id="navbarScrollingDropdown"
-              >
-                <NavDropdown.Item href="#action3">Cakes</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">Tarts</NavDropdown.Item>
-                <NavDropdown.Item href="#action5">Truffles</NavDropdown.Item>
-              </NavDropdown>
-              <Link to="/shop" className="item-nav-bar">
-                <Nav.Item className="item-nav-bar">SHOP</Nav.Item>
-              </Link>
-              <Link to="/contact" className="item-nav-bar">
-                <Nav.Item className="item-nav-bar">CONTACT</Nav.Item>
-              </Link>
-              <div className="d-flex">
-                <Link to="/login" className="item-nav-bar">
-                  <FaRegUser className="item-nav-login" />
-                </Link>
-                <Link to="/cart" className="item-nav-bar">
-                  <AiOutlineShopping className="item-nav-cart" />
-                </Link>
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Container>
-    </Navbar>
-  );
+	return (
+		<Navbar bg='light' className='nav-container navbar-me-auto ' sticky='top'>
+			<Container fluid className='d-flex justify-content-between'>
+				<div className='logo-container'>
+					<Link to='/'>
+						<Navbar.Brand>
+							<GiCupcake className='logo' />
+						</Navbar.Brand>
+					</Link>
+				</div>
+				<div className='container-nav-items'>
+					<Navbar.Toggle aria-controls='navbarScroll' />
+					<Navbar.Collapse id='navbarScroll'>
+						<Nav
+							className='me-auto my-2 my-lg-0'
+							style={{ maxHeight: "100px" }}
+							navbarScroll>
+							<Link to='/' className='item-nav-bar'>
+								<Nav.Item>HOME</Nav.Item>
+							</Link>
+							<Link to='/about' className='item-nav-bar'>
+								<Nav.Item className='item-nav-bar'>ABOUT</Nav.Item>
+							</Link>
+							<NavDropdown
+								className='item-nav-bar'
+								title='CATEGORIES'
+								id='navbarScrollingDropdown'>
+								<NavDropdown.Item className='dropdown-text' href='#action3'>
+									Cakes
+								</NavDropdown.Item>
+								<NavDropdown.Item className='dropdown-text' href='#action4'>
+									Tarts
+								</NavDropdown.Item>
+								<NavDropdown.Item className='dropdown-text' href='#action5'>
+									Truffles
+								</NavDropdown.Item>
+							</NavDropdown>
+							<Link to='/shop' className='item-nav-bar'>
+								<Nav.Item className='item-nav-bar'>SHOP</Nav.Item>
+							</Link>
+							<Link to='/contact' className='item-nav-bar'>
+								<Nav.Item className='item-nav-bar'>CONTACT</Nav.Item>
+							</Link>
+							<div className='d-flex'>
+								<Link to='/login' className='item-nav-bar'>
+									<FaRegUser className='item-nav-login' />
+								</Link>
+								<Link to='/cart' className='item-nav-bar'>
+									<AiOutlineShopping className='item-nav-cart' />
+								</Link>
+							</div>
+						</Nav>
+					</Navbar.Collapse>
+				</div>
+			</Container>
+		</Navbar>
+	);
 }
 
 export default NavComp;
