518d2baa990969733f1c3f5fd05f0d0c1c6c05a3
flofal <florencia.falco93@gmail.com>
2022-06-28 16:21:05 -0300

    login

--- src/components/loginRegister/LoginRegister.jsx
+++ src/components/loginRegister/LoginRegister.jsx
@@ -1,12 +1,11 @@
 import "./login-register.css";
-
+import "./login"
 import Offcanvas from "react-bootstrap/Offcanvas";
 import { useState } from "react";
 
 function LoginRegister({ show, setShow }) {
   const handleClose = () => setShow(false);
-  const [input, setInput] = useState("");
-  const [input2, setInput2] = useState("");
+
 
   return (
     <Offcanvas show={show} onHide={handleClose} placement="end" name="end">
@@ -14,34 +13,7 @@ function LoginRegister({ show, setShow }) {
         <Offcanvas.Title></Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        {/* <div className="login-container">
-          <h1 className="title-offcanvas">LOG IN</h1>
-          <p className="subtitle-offcanvas">
-            Welcome to Cakes Shop, please put your login credentials below to
-            start shopping.
-          </p>
-          <div>
-            <label htmlFor="">Email</label>
-            <input
-              className="input"
-              type="text"
-              value={input}
-              placeholder="example@example.com"
-              onChange={(event) => setInput(event.target.value)}
-            />
-            <label htmlFor="">Password</label>
-            <input
-              className="input"
-              type="text"
-              value={input2}
-              placeholder="Password"
-              onChange={(event) => setInput2(event.target.value)}
-            />
-          </div>
-          <button className="button-login">Log in</button>
-          <small>Don't have an account?</small>
-          <button className="button-signup">Sign up for free.</button>
-        </div> */}
+        
         <div className="register-container">
           <h1 className="title-offcanvas">SIGN UP</h1>
           <p className="subtitle-offcanvas">
@@ -49,6 +21,22 @@ function LoginRegister({ show, setShow }) {
             start shopping.
           </p>
           <div>
+          <label htmlFor="">Name</label>
+            <input
+              className="input"
+              type="text"
+              value={input}
+              placeholder="Jane"
+              onChange={(event) => setInput(event.target.value)}
+            />
+            <label htmlFor="">Last Name</label>
+            <input
+              className="input"
+              type="text"
+              value={input}
+              placeholder="Doe"
+              onChange={(event) => setInput(event.target.value)}
+            />
             <label htmlFor="">Email</label>
             <input
               className="input"
