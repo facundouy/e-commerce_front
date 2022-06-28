07907ea51db75654e4b17ca570689800d8547ed0
flofal <florencia.falco93@gmail.com>
2022-06-28 12:11:26 -0300

    login update

--- src/components/loginRegister/LoginRegister.jsx
+++ src/components/loginRegister/LoginRegister.jsx
@@ -1,9 +1,12 @@
 import "./login-register.css";
 
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { useState } from "react";
 
 function LoginRegister({ show, setShow }) {
   const handleClose = () => setShow(false);
+  const [input, setInput] = useState("");
+  const [input2, setInput2] = useState("");
 
   return (
     <Offcanvas show={show} onHide={handleClose} placement="end" name="end">
@@ -11,14 +14,62 @@ function LoginRegister({ show, setShow }) {
         <Offcanvas.Title></Offcanvas.Title>
       </Offcanvas.Header>
       <Offcanvas.Body>
-        <h1>Create an account</h1>
-        <span>Create an account</span>
-        <label htmlFor="">Name</label>
-        <input type="text" />
-        <label htmlFor="">Email</label>
-        <input type="text" />
-        <label htmlFor="">Password</label>
-        <input type="text" />
+        {/* <div className="login-container">
+          <h1 className="title-offcanvas">LOG IN</h1>
+          <p className="subtitle-offcanvas">
+            Welcome to Cakes Shop, please put your login credentials below to
+            start shopping.
+          </p>
+          <div>
+            <label htmlFor="">Email</label>
+            <input
+              className="input"
+              type="text"
+              value={input}
+              placeholder="example@example.com"
+              onChange={(event) => setInput(event.target.value)}
+            />
+            <label htmlFor="">Password</label>
+            <input
+              className="input"
+              type="text"
+              value={input2}
+              placeholder="Password"
+              onChange={(event) => setInput2(event.target.value)}
+            />
+          </div>
+          <button className="button-login">Log in</button>
+          <small>Don't have an account?</small>
+          <button className="button-signup">Sign up for free.</button>
+        </div> */}
+        <div className="register-container">
+          <h1 className="title-offcanvas">SIGN UP</h1>
+          <p className="subtitle-offcanvas">
+            Welcome to Cakes Shop, please put your login credentials below to
+            start shopping.
+          </p>
+          <div>
+            <label htmlFor="">Email</label>
+            <input
+              className="input"
+              type="text"
+              value={input}
+              placeholder="example@example.com"
+              onChange={(event) => setInput(event.target.value)}
+            />
+            <label htmlFor="">Password</label>
+            <input
+              className="input"
+              type="text"
+              value={input2}
+              placeholder="Password"
+              onChange={(event) => setInput2(event.target.value)}
+            />
+          </div>
+          <button className="button-login">Sign in</button>
+          <small>Alredy have an account?</small>
+          <button className="button-signup">Log in.</button>
+        </div>
       </Offcanvas.Body>
     </Offcanvas>
   );
