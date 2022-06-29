48f9d6675efa71831d241d269529905c49075015
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-06-29 10:32:12 -0300

    configuración de persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,45 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./userSlice";
 import cartReducer from "./cartSlice";
 
-export const store = configureStore(
-  {
-    reducer: {
-      user: userReducer,
-      cart: cartReducer,
-    },
-  },
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //Esto es para que funcione el redux devtools
-);
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = { key: "root", storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+export const persistor = persistStore(store);
+
+// export const store = configureStore(
+//   {
+//     reducer: {
+//       user: userReducer,
+//       cart: cartReducer,
+//     },
+//   },
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() //Esto es para que funcione el redux devtools
+// );
