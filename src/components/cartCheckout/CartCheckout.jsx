deb0a9f253223e0187282fff92933dbef793ab92
Santiago Fantoni <74023431+SantiagoFantoni@users.noreply.github.com>
2022-06-30 12:42:01 -0300

    Update CartCheckout.jsx

--- src/components/cartCheckout/CartCheckout.jsx
+++ src/components/cartCheckout/CartCheckout.jsx
@@ -1,20 +1,34 @@
+import { useSelector } from "react-redux";
 import "./cartCheckout.css";
+import axios from "axios"
+
 
 function CartCheckout({ total }) {
+	const items = useSelector(state=>state.cart)
+	const checkOutHandler = ()=>{
+	const response = async()=>{
+		await axios.patch(`${process.env.REACT_APP_BACKEND_URL}/api/product`,{items:items})
+		
+	}
+	console.log(response.data)
+	}
 	return (
 		<div className='checkout-container'>
 			<h2 className='checkout-title'>CART TOTALS</h2>
 			<table className='checkout-table'>
 				<tr className='checkout-table-row'>
 					<td className='checkout-table-cell'>Subtotal</td>
-					<td className='checkout-table-cell'>${total}</td>
+					<td className='check out-table-cell'>${total}</td>
 				</tr>
 				<tr className='checkout-table-row'>
 					<td className='checkout-table-cell'>Total</td>
 					<td className='checkout-table-cell'>${total}</td>
 				</tr>
 			</table>
-			<button className='checkout-btn'>PROCEED TO CHECKOUT</button>
+			
+			<button className='checkout-btn' onClick={()=>{checkOutHandler(); }}>PROCEED TO CHECKOUT</button>
+			
+			
 		</div>
 	);
 }
