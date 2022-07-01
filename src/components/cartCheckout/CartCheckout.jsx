4c46d85aed0ed1a578dbd19b829842b5aeaa6bbd
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-07-01 14:14:22 -0300

    createOrder

--- src/components/cartCheckout/CartCheckout.jsx
+++ src/components/cartCheckout/CartCheckout.jsx
@@ -1,36 +1,62 @@
 import { useSelector } from "react-redux";
 import "./cartCheckout.css";
-import axios from "axios"
-
+import axios from "axios";
 
 function CartCheckout({ total }) {
-	const items = useSelector(state=>state.cart)
-	const checkOutHandler = ()=>{
-	const response = async()=>{
-		await axios.patch(`${process.env.REACT_APP_BACKEND_URL}/api/product`,{items:items})
-		
-	}
-	console.log(response.data)
-	}
-	return (
-		<div className='checkout-container'>
-			<h2 className='checkout-title'>CART TOTALS</h2>
-			<table className='checkout-table'>
-				<tr className='checkout-table-row'>
-					<td className='checkout-table-cell'>Subtotal</td>
-					<td className='check out-table-cell'>${total}</td>
-				</tr>
-				<tr className='checkout-table-row'>
-					<td className='checkout-table-cell'>Total</td>
-					<td className='checkout-table-cell'>${total}</td>
-				</tr>
-			</table>
-			
-			<button className='checkout-btn' onClick={()=>{checkOutHandler(); }}>PROCEED TO CHECKOUT</button>
-			
-			
-		</div>
-	);
+  const cart = useSelector((state) => state.cart);
+  const checkOutHandler = () => {
+    const response = async () => {
+      await axios.patch(`${process.env.REACT_APP_BACKEND_URL}/api/product`, {
+        items: cart,
+      });
+    };
+    console.log(response.data);
+  };
+
+  async function createOrder(event) {
+    const productsName = [];
+    for (const product of cart) {
+      productsName.push(product.name);
+    }
+    const productsQuantity = [];
+    for (const product of cart) {
+      productsQuantity.push(product.quantity);
+    }
+    const response = await axios.post(
+      `${process.env.REACT_APP_BACKEND_URL}/create/order`,
+      {
+        productsName: productsName,
+        productsQuantity: productsQuantity,
+      }
+    );
+    console.log(response.data);
+  }
+
+  return (
+    <div className="checkout-container">
+      <h2 className="checkout-title">CART TOTALS</h2>
+      <table className="checkout-table">
+        <tr className="checkout-table-row">
+          <td className="checkout-table-cell">Subtotal</td>
+          <td className="check out-table-cell">${total}</td>
+        </tr>
+        <tr className="checkout-table-row">
+          <td className="checkout-table-cell">Total</td>
+          <td className="checkout-table-cell">${total}</td>
+        </tr>
+      </table>
+
+      <button
+        className="checkout-btn"
+        onClick={() => {
+          checkOutHandler();
+          createOrder();
+        }}
+      >
+        PROCEED TO CHECKOUT
+      </button>
+    </div>
+  );
 }
 
 export default CartCheckout;
