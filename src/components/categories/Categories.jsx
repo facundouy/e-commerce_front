4a2907f4d63b554327102fe1cbd59f79502d73e7
paulinairazusta <104213023+paulinairazusta@users.noreply.github.com>
2022-07-08 12:34:20 -0300

    update categories
    Condición para que no se traiga los productos de x categoría si esta se borra

--- src/components/categories/Categories.jsx
+++ src/components/categories/Categories.jsx
@@ -5,53 +5,57 @@ import ShopBanner from "../shopBanner/ShopBanner";
 import "./categories.css";
 
 function Categories() {
-	const params = useParams();
-	const productList = useSelector((state) => state.product);
-	const filteredProducts = productList.filter((product) => {
-		return product.category.name === params.name;
-	});
+  const params = useParams();
+  const productList = useSelector((state) => state.product);
+  const filteredProducts = productList.filter((product) => {
+    return product.category && product.category.name === params.name;
+  });
 
-	return (
-		<>
-			<ShopBanner bannerTitle={`${params.name}`} />
-			<div className='products-container'>
-				<ul className='products-list'>
-					{filteredProducts.map((product) => {
-						return (
-							<div key={product._id} className='card-product'>
-								<Link to={`/product/${product.slug}`}>
-									<img
-										src={`https://tkyarzymrutnhhccfvhu.supabase.co/storage/v1/object/public/images/${product.image}`}
-										alt='cake cover'
-									/>
-								</Link>
+  return (
+    <>
+      <ShopBanner bannerTitle={`${params.name}`} />
+      <div className="products-container">
+        <ul className="products-list">
+          {filteredProducts.map((product) => {
+            return (
+              product.category && (
+                <div key={product._id} className="card-product">
+                  <Link to={`/product/${product.slug}`}>
+                    <img
+                      src={`https://tkyarzymrutnhhccfvhu.supabase.co/storage/v1/object/public/images/${product.image}`}
+                      alt="cake cover"
+                    />
+                  </Link>
 
-								<div className='product-info'>
-									<Link
-										className='title-product'
-										to={`/product/${product.slug}`}>
-										<h6 className='title-product'>
-											{product.name.toUpperCase()}
-										</h6>
-									</Link>
-									<Link
-										className='text-decoration-none text-dark'
-										to={`category/${product.category.name}`}>
-										<span className='product-category'>
-											{product.category.name}
-										</span>
-									</Link>
+                  <div className="product-info">
+                    <Link
+                      className="title-product"
+                      to={`/product/${product.slug}`}
+                    >
+                      <h6 className="title-product">
+                        {product.name.toUpperCase()}
+                      </h6>
+                    </Link>
+                    <Link
+                      className="text-decoration-none text-dark"
+                      to={`category/${product.category.name}`}
+                    >
+                      <span className="product-category">
+                        {product.category.name}
+                      </span>
+                    </Link>
 
-									<span className='product-price'>${product.price}</span>
-								</div>
-							</div>
-						);
-					})}
-				</ul>
-				<SideContainer />
-			</div>
-		</>
-	);
+                    <span className="product-price">${product.price}</span>
+                  </div>
+                </div>
+              )
+            );
+          })}
+        </ul>
+        <SideContainer />
+      </div>
+    </>
+  );
 }
 
 export default Categories;
