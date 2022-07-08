97d790f1016f69b3af1bd0d24ea1a63844f90a82
facundouy <97556086+facundouy@users.noreply.github.com>
2022-07-08 00:21:55 -0300

    Responsiveness About

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -114,25 +114,27 @@ const About = () => {
         </div>
 
         <div className="stats-container">
-          <div className="number-container">
-            <span>3</span>
-            <h5>WEEKS</h5>
+          <div className="stats-subcontainer">
+            <div className="number-container">
+              <span>3</span>
+              <h5>WEEKS</h5>
+            </div>
+            <div className="number-container">
+              <span>600+</span>
+              <h5>STAFF-HOURS</h5>
+            </div>
           </div>
-          <div className="number-container">
-            <span>+600</span>
-            <h5>HOURS</h5>
-          </div>
-          <div className="number-container">
-            <span>4</span>
-            <h5>COLABORATORS</h5>
-          </div>
-          <div className="number-container">
-            <span>250</span>
-            <h5>COMMITS</h5>
+          <div className="stats-subcontainer">
+            <div className="number-container">
+              <span>4</span>
+              <h5>COLABORATORS</h5>
+            </div>
+            <div className="number-container">
+              <span>400+</span>
+              <h5>COMMITS</h5>
+            </div>
           </div>
         </div>
-
-      
       </div>
     </div>
   );
